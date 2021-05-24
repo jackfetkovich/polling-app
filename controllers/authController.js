b490f2e0e943a4472e86c027dda83a49c5fe9aa1
Jack Fetkovich <johnfetkovich1014@gmail.com>
2021-05-24 16:37:19 -0400

    Implement user login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 const signToken = id => {
 	return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -8,29 +10,48 @@ const signToken = id => {
 	});
 };
 
-exports.signup = async (req, res, next) => {
-	try {
-		const user = await User.create({
-			username: req.body.username,
-			email: req.body.email,
-			password: req.body.password,
-			passwordConfirm: req.body.passwordConfirm,
-		});
+exports.signup = catchAsync(async (req, res, next) => {
+	const user = await User.create({
+		username: req.body.username,
+		email: req.body.email,
+		password: req.body.password,
+		passwordConfirm: req.body.passwordConfirm,
+	});
 
-		const token = signToken(user._id);
-    
+	const token = signToken(user._id);
 
-		res.status(201).json({
-			status: 'success',
-			token,
-			data: {
-				user,
-			},
-		});
-	} catch (e) {
-		res.status(400).json({
-			status: 'fail',
-			message: e.message,
-		});
+	res.status(201).json({
+		status: 'success',
+		token,
+		data: {
+			user,
+		},
+	});
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+	// Extracts username and password from request
+	const { username, password } = req.body;
+
+	// Makes sure that a username and password were both entered
+	if (!username || !password) {
+		return next(new AppError('Please enter username and password', 400));
 	}
-};
+
+	// Find the user in database (make sure password comes with it for comparison)
+	const user = await User.findOne({ username }).select('+password');
+
+	// Make sure the user exists and their password is correct
+	if (!user || !await user.correctPassword(password, user.password)) {
+		return next(new AppError('Incorrect username or password', 401));
+	}
+
+	// If everything checks out, sign off on the token
+	const token = signToken(user._id);
+	
+	//Send back the token
+	res.status(200).json({
+		status: 'success',
+		token,
+	});
+});
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,6 +5,7 @@ const authController = require('../controllers/authController');
 const userController = require('../controllers/userController');
 
 router.post('/signup', authController.signup);
+router.post('/login', authController.login);
 router.get('/', userController.getAllUsers );
 router.get('/:id', userController.getUser);
 
